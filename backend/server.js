a8c4453fdf65b3d2a343b9ea348d84d95c91965e
Tarunkagkag <tarunkag4@gmail.com>
2026-01-05 14:02:29 +0530

    Added

--- backend/server.js
+++ backend/server.js
@@ -72,6 +72,21 @@ app.use(
   })
 );
 
+app.use(
+  cors({
+    origin: function(origin, callback) {
+      // allow requests with no origin like mobile apps or curl
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = `The CORS policy for this site does not allow access from the specified Origin: ${origin}`;
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true,
+  })
+);
 /* =======================
    ROUTES
 ======================= */
